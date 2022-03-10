379e2abf62c516eeb14614fb2dbdd4d3d8f99506
storesource <36395224+storesource@users.noreply.github.com>
2022-03-10 21:07:43 +0530

    Add files via upload

---
+++ src/main/java/io/storesource/reactiveflows/ReactiveFlowsApplication.java
@@ -0,0 +1,13 @@
+package io.storesource.reactiveflows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveFlowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveFlowsApplication.class, args);
+	}
+
+}
